8fe0cc216dd080d04ec98cf813777c927f7c6d0a
Jan-Piet Mens <jp@mens.de>
2026-03-06 12:47:24 +0000

    configuration for packaging for Ubuntu 25.10 	addresses https://github.com/owntracks/quicksetup/issues/97

---
+++ etc/ubuntu/fpm-make.sh
@@ -0,0 +1,54 @@
+#!/bin/sh
+
+set -e
+
+tempdir=$(mktemp -d /tmp/ot-XXX)
+
+make install DESTDIR=$tempdir
+
+install -D -m644 README.md $tempdir/usr/share/doc/ot-recorder/README.md
+install -D -m644 etc/ot-recorder.service $tempdir/etc/systemd/system/ot-recorder.service
+
+name="ot-recorder"
+version="$(awk 'NR==1 {print $NF;}' version.h | sed -e 's/"//g' )-0-deb"
+arch=$(uname -m)
+
+case $arch in
+	armv7l) arch=armhf;;
+esac
+
+debfile="${name}_${version}_${arch}.deb"
+
+rm -f "${debfile}"
+
+libcurl='libcurl4t64'
+libsodium='libsodium23'
+liblua='liblua5.4-0'
+libconfig='libconfig11'
+
+fpm -s dir \
+        -t deb \
+        -n ${name} \
+        -v ${version} \
+        --vendor "OwnTracks.org" \
+        -a "${arch}" \
+        --maintainer 'jpmens@gmail.com' \
+	--deb-no-default-config-files \
+        --description "A lightweight back-end for consuming OwnTracks data from an MQTT broker" \
+        --license "https://github.com/owntracks/recorder/blob/master/LICENSE" \
+        --url "http://owntracks.org" \
+        -C $tempdir \
+        -p ${debfile} \
+        -d "${libcurl}" \
+        -d "libmosquitto1" \
+        -d "${liblua}" \
+        -d "${libconfig}" \
+        -d "${libsodium}" \
+        -d "liblmdb0" \
+        -d "libuuid1" \
+	--config-files etc/default/ot-recorder \
+        --post-install etc/debian/postinst \
+        usr var etc
+
+echo "${debfile}" > package.name
+rm -rf "${tempdir}"
